dc6df49e5548ede5976c3e02d88c4a969db86d3c
misilelab <misileminecord@gmail.com>
2025-04-10 20:21:45 +0900

    infisical: 0.36.23 -> 0.39.1
    https://github.com/Infisical/infisical/compare/infisical-cli/v0.36.23...infisical-cli/v0.39.1
    
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- pkgs/development/tools/infisical/default.nix
+++ pkgs/development/tools/infisical/default.nix
@@ -21,7 +21,7 @@ let
   buildHashes = builtins.fromJSON (builtins.readFile ./hashes.json);
 
   # the version of infisical
-  version = "0.36.23";
+  version = "0.39.1";
 
   # the platform-specific, statically linked binary
   src =
