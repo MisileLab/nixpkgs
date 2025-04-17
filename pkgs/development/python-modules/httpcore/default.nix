10b2b2a80d1e9de98599824a43d2bb8a82457a0c
misilelab <misileminecord@gmail.com>
2025-04-17 15:20:40 +0900

    python3Packages.httpcore: 1.0.7 -> 1.0.8
    https://github.com/encode/httpcore/blob/1.0.8/CHANGELOG.md
    
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- pkgs/development/python-modules/httpcore/default.nix
+++ pkgs/development/python-modules/httpcore/default.nix
@@ -24,7 +24,7 @@
 
 buildPythonPackage rec {
   pname = "httpcore";
-  version = "1.0.7";
+  version = "1.0.8";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -33,7 +33,7 @@ buildPythonPackage rec {
     owner = "encode";
     repo = "httpcore";
     tag = version;
-    hash = "sha256-hEGKYJnsTjygRu++lNrPWfx/xiUR7Cp+DTUVPa0m5fM=";
+    hash = "sha256-Klr74IlL5x4uQlYYFjCVbwWQkNsmc+4OJMNP9AUDUCk=";
   };
 
   build-system = [
